639ed37728986a6ccccefe056b760389a54f6f14
Victor Mours <victor.mours@gmail.com>
2014-08-06 18:38:52 +0200

    Added static server to chaplin

--- chaplin/lib/chaplin/server.rb
+++ chaplin/lib/chaplin/server.rb
@@ -7,21 +7,20 @@ module Chaplin
     end
 
     def call(rack_env)
-      puts "receiving request"
+      puts "Calling Chaplin server"
       request = Rack::Request.new(rack_env)
       template_name = @router.template_for(request)
-      puts "templates: #{template_name}"
-      response_body = ''
       if template_name
         response_body = @renderer.render(request, template_name)
+
+        status = 200
+        headers = {}
+        body = [response_body]
+
+        [status, headers, body]
+      else
+        [404, {}, ["Not found"]]
       end
-      puts "response_body: #{response_body}"
-
-      status = 200
-      headers = {}
-      body = [response_body]
-
-      [status, headers, body]
     end
 
   end
