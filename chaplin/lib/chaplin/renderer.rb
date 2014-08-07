b7457b368f2eb7c079678831869bbd5b48d59b9d
Victor Mours <victor.mours@gmail.com>
2014-08-07 11:38:30 +0200

    Working layout

--- chaplin/lib/chaplin/renderer.rb
+++ chaplin/lib/chaplin/renderer.rb
@@ -9,6 +9,16 @@ module Chaplin
     end
 
     def render(json_data, template_name)
+      puts "rendering stuff"
+      page = Mustache.new
+      page.template_file = @templates_path + 'layout.html'
+      page_data = { content: render_page(json_data, template_name) }
+      page.render(page_data)
+    end
+
+    private
+
+    def render_page(json_data, template_name)
       page = Mustache.new
       page.template_file = @templates_path + template_name
       page.render(json_data)
